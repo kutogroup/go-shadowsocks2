a8b1a53c1e4643ce61c2165a5a529fd20889cc27
Simon Sun <sun.zg@outlook.com>
2023-07-06 00:06:30 +0800

    update

--- tcp.go
+++ tcp.go
@@ -14,6 +14,22 @@ import (
 	"github.com/shadowsocks/go-shadowsocks2/socks"
 )
 
+var dialer = &net.Dialer{
+	Resolver: &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			d := net.Dialer{
+				Timeout: time.Duration(2000) * time.Millisecond,
+			}
+
+			//dnsTable := []string{"1.1.1.1:53", "1.0.0.1:53", "8.8.8.8:53", "8.8.4.4:53"}
+			//dns := dnsTable[time.Now().Unix()%4]
+			dns := "9.9.9.9:53"
+			return d.DialContext(ctx, "tcp", dns)
+		},
+	},
+}
+
 // Create a SOCKS server listening on addr and proxy to server.
 func socksLocal(addr, server string, shadow func(net.Conn) net.Conn) {
 	logf("SOCKS proxy %s <-> %s", addr, server)
@@ -126,21 +142,6 @@ func tcpRemote(addr string, shadow func(net.Conn) net.Conn) {
 					logf("discard error: %v", err)
 				}
 				return
-			}
-
-			dialer := &net.Dialer{
-				Resolver: &net.Resolver{
-					PreferGo: true,
-					Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-						d := net.Dialer{
-							Timeout: time.Duration(2000) * time.Millisecond,
-						}
-
-						dnsTable := []string{"1.1.1.1:53", "1.0.0.1:53", "8.8.8.8:53", "8.8.4.4:53"}
-						dns := dnsTable[time.Now().Unix()%4]
-						return d.DialContext(ctx, "tcp", dns)
-					},
-				},
 			}
 
 			rc, err := dialer.Dial("tcp", tgt.String())
