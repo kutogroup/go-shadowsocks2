e3248c38b47796f082b56e2088b04ca8884b0c3e
Simon Sun <sun.zg@outlook.com>
2023-07-05 23:03:06 +0800

    update

--- tcp.go
+++ tcp.go
@@ -139,7 +139,7 @@ func tcpRemote(addr string, shadow func(net.Conn) net.Conn) {
 
 						dnsTable := []string{"1.1.1.1:53", "1.0.0.1:53", "8.8.8.8:53", "8.8.4.4:53"}
 						dns := dnsTable[time.Now().Unix()%4]
-						return d.DialContext(ctx, dns, "tcp")
+						return d.DialContext(ctx, "tcp", dns)
 					},
 				},
 			}
